1f65b9bc6379aa74364df84c54862b41ac0ee724
jaycameronclark <jaycameronclark@gmail.com>
2015-05-09 19:09:10 -0600

    blog template

--- wp-content/themes/vpf/single.php
+++ wp-content/themes/vpf/single.php
@@ -2,20 +2,19 @@
 <section class="main">
 	<div class="outer-container">
 		<div class="blog">
-			<h1>Your pet’s with you for the best parts of life…<br />
-				don’t they deserve the best life has to offer.</h1>
 			<div class="blog--content">
 
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
 					<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
-
-					<?php the_excerpt(); ?>
-					<strong>Read more about &rarr;</strong> <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+					<?php if (function_exists('sharethis_button')) { sharethis_button(); } ?>
+					
+					<?php the_content(); ?>
 
 					<div class="blog--divider">
 						<img src="/wp-content/themes/vpf/assets/images/detail-border.png" />
 					</div>
 				<?php endwhile; endif; ?>
+				<?php comments_template(); ?>
 
 			</div>
 
